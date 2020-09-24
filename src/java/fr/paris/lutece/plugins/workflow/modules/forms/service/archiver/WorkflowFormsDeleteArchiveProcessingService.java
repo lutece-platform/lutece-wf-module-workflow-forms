4d9479348bad4d27a60d3e6ce651a92a0ee94644
Guillaume Lépine <guillaume.lepine@sogeti.com>
2020-09-24 10:59:23 +0200

    mvn license & format

--- src/java/fr/paris/lutece/plugins/workflow/modules/forms/service/archiver/WorkflowFormsDeleteArchiveProcessingService.java
+++ src/java/fr/paris/lutece/plugins/workflow/modules/forms/service/archiver/WorkflowFormsDeleteArchiveProcessingService.java
@@ -1,3 +1,36 @@
+/*
+ * Copyright (c) 2002-2020, City of Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
 package fr.paris.lutece.plugins.workflow.modules.forms.service.archiver;
 
 import java.util.List;
@@ -24,28 +57,29 @@ import fr.paris.lutece.plugins.workflowcore.service.task.ITask;
 public class WorkflowFormsDeleteArchiveProcessingService extends AbstractArchiveProcessingService
 {
     public static final String BEAN_NAME = "workflow-forms.workflowFormsDeleteArchiveProcessingService";
-    
+
     private static final String TASK_TYPE_EDITFORMS = "editFormResponseTypeTask";
     private static final String TASK_TYPE_COMPLETEFORMS = "completeFormResponseTypeTask";
     private static final String TASK_TYPE_RESUBMITFORMS = "resubmitFormResponseTypeTask";
-    
+
     @Inject
     private IResubmitFormResponseService _resubmitFormResponseService;
-    
+
     @Inject
     private ICompleteFormResponseService _completeFormResponseService;
-    
+
     @Inject
     private IEditFormResponseTaskHistoryService _editFormResponseTaskHistoryService;
-    
+
     @Inject
     private FormService _formService;
-    
+
     @Override
     public void archiveResource( ResourceWorkflow resourceWorkflow )
     {
-        List<ResourceHistory> historyList = _resourceHistoryService.getAllHistoryByResource( resourceWorkflow.getIdResource( ), resourceWorkflow.getResourceType( ), resourceWorkflow.getWorkflow( ).getId( ) );
-        
+        List<ResourceHistory> historyList = _resourceHistoryService.getAllHistoryByResource( resourceWorkflow.getIdResource( ),
+                resourceWorkflow.getResourceType( ), resourceWorkflow.getWorkflow( ).getId( ) );
+
         archiveTaskEditForms( historyList );
         archiveTaskCompleteForms( historyList );
         archiveTaskResubmitForms( historyList );
@@ -63,7 +97,7 @@ public class WorkflowFormsDeleteArchiveProcessingService extends AbstractArchive
             }
         }
     }
-    
+
     private void archiveTaskCompleteForms( List<ResourceHistory> historyList )
     {
         for ( ResourceHistory history : historyList )
@@ -75,7 +109,7 @@ public class WorkflowFormsDeleteArchiveProcessingService extends AbstractArchive
             }
         }
     }
-    
+
     private void archiveTaskResubmitForms( List<ResourceHistory> historyList )
     {
         for ( ResourceHistory history : historyList )
@@ -87,21 +121,21 @@ public class WorkflowFormsDeleteArchiveProcessingService extends AbstractArchive
             }
         }
     }
-    
+
     private void archiveFormResponse( ResourceWorkflow resourceWorkflow )
     {
         int formResponseId = resourceWorkflow.getIdResource( );
         FormResponse formResponse = FormResponseHome.loadById( formResponseId );
-        
+
         for ( FormQuestionResponse formQuestionResponse : FormQuestionResponseHome.getFormQuestionResponseListByFormResponse( formResponseId ) )
         {
             FormQuestionResponseHome.remove( formQuestionResponse );
         }
 
         FormResponseStepHome.removeByFormResponse( formResponseId );
-        
+
         FormResponseHome.remove( formResponseId );
-        
+
         _formService.fireFormResponseEventDelete( formResponse );
     }
 }
